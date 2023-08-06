3238a69c9e0eac758ce280cea4d8d1e0065bebad
mr-louback <loubacked4work@outlook.com>
2023-08-06 12:41:10 -0300

    new: UserController, mod: AdminControLoginUserModl

---
+++ system/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace system\Controller;
+
+use system\Model\UserModel;
+use system\Nucleus\Helpers;
+use system\Nucleus\Controller;
+use system\Nucleus\Session;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct('layouts/site/views');
+    }
+
+   public static function sessionIdUser(){
+    $session = (new Session());
+    if(!$session->sessionCheck('userId')){
+        return null;
+    }
+    return (new UserModel())->getUserId($session->userId);
+   }
+}
